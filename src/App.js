6bf1c121b6088ee3e354e2cca137dd68fcd6e1bb
Seyi_Coded <opadonuseyi01@gmail.com>
2023-05-10 14:39:29 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{ margin: 40, display: "flex", alignItems: 'center', flexDirection: 'column' }}>
+      <img style={{ 
+        // width: 32,
+        height: 32
+       }} src="https://s.yimg.com/rz/p/yahoo_frontpage_en-US_s_f_p_bestfit_frontpage_2x.png" />
+
+       <br />
+       <p>Uh ooh</p>
+       <p style={{ textAlign: 'center' }}>Look like something went wrong. <br /> Please try again later</p>
+    </div>
+  );
+}
+
+export default App;
